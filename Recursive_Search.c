429a4d03cccaacfb740d521d53391c6fd18feb78
Genato <druidhr@gmail.com>
2016-09-26 22:08:54 +0200

    Add files via upload

---
+++ Recursive_Search.c
@@ -0,0 +1,51 @@
+﻿/* PATH Example --> C:\\Users\\Korisnik\\Pictures\\*  */
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void RecursiveFileSearch();
+ 
+int main()
+{
+	char start_path[MAX_PATH] = "E:\\PROGRAMMING\\C++\\LAN_spy_GUI\\*";
+
+	RecursiveFileSearch(start_path);
+}
+
+void RecursiveFileSearch(char* p)
+{
+	char path[MAX_PATH];
+	WIN32_FIND_DATAA findfiledata;
+	HANDLE hFind = INVALID_HANDLE_VALUE;
+
+	strcpy(path, p);
+
+	if((hFind = FindFirstFileA(path, &findfiledata)) == INVALID_HANDLE_VALUE)
+	{
+		printf ("FindFirstFile failed (%d) \n", GetLastError());
+	}
+
+	if(strchr(findfiledata.cFileName, '.')) 
+	{
+		FindNextFileA(hFind, &findfiledata);
+	}
+
+	while(FindNextFileA(hFind, &findfiledata))
+	{
+		if(findfiledata.dwFileAttributes == 38)
+			FindNextFileA(hFind, &findfiledata);
+
+		if(findfiledata.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+		{
+			printf("%s\n",findfiledata.cFileName);
+			RecursiveFileSearch(strcat(strcat(strcat(strtok(path, "*"), findfiledata.cFileName), "\\"), "*"));
+			strcpy(path, p);
+		}
+
+		if(!(findfiledata.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY))
+		{
+			printf("%s\n",findfiledata.cFileName);
+		}
+	}
+		
+}
